55d5131b2bc187af7515a2d446de28f9c850b6fd
João Guilherme <joaoguiefernandes@gmail.com>
2022-10-22 00:49:15 -0300

    Atualização final.
    Teste não será continuado por enquanto.

--- classe/Temperatura.class.php
+++ classe/Temperatura.class.php
@@ -4,10 +4,18 @@
         
         private $id;
         private $temp;
+        private $acx;
+        private $acy;
+        private $gyx;
+        private $gyy;
         
-        public function __construct($id,$temp){
+        public function __construct($id,$temp,$acx,$acy,$gyx,$gyy){
             $this->setId($id);
             $this->setTemp($temp);
+            $this->setAcx($acx);
+            $this->setAcy($acy);
+            $this->setGyx($gyx);
+            $this->setGyy($gyy);
         }
 
         public function setId($id){if($id > 0){$this->id = $id;}}
@@ -15,11 +23,28 @@
 
         public function setTemp($temp){if($temp != ""){$this->temp = $temp;}}
         public function getTemp(){return $this->temp;}
+
+        public function setAcx($acx){if($acx != ""){$this->acx = $acx;}}
+        public function getAcx(){return $this->acx;}
+
+        public function setAcy($acy){if($acy != ""){$this->acy = $acy;}}
+        public function getAcy(){return $this->acy;}
+
+        public function setGyx($gyx){if($gyx != ""){$this->gyx = $gyx;}}
+        public function getGyx(){return $this->gyx;}
+
+        public function setGyy($gyy){if($gyy != ""){$this->gyy = $gyy;}}
+        public function getGyy(){return $this->gyy;}
         
         public function Salvar(){
             try{
-                $sql = "INSERT INTO `ifc`.`temp_table` (`temp_value`) VALUES (:temp)";
-                $param = array( ":temp" => $this->getTemp());
+                $sql = "INSERT INTO `ifc`.`temp_table` (`temp_value`, `AcY`, `AcX` , `GyX`, `GyY`) 
+                        VALUES (:temp, :acy, :acx, :gyx, :gyy)";
+                $param = array( ":temp" => $this->getTemp(),
+                                ":acy" => $this->getAcy(),
+                                ":acx" => $this->getAcx(),
+                                ":gyx" => $this->getGyx(),
+                                ":gyy" => $this->getGyy());
                 $teste = parent::Execute($sql,$param);
                 return $teste;
             }catch(Exception $e){
@@ -28,8 +53,10 @@
         }
 
         public function __toString(){
-            return "ID: ".$this->getId()."<br>
-                    Temperatura: ".$this->getTemp();    
+            return "ID: ".$this->getId() ."<br>
+                    Temperatura: ".$this->getTemp() ."<br>
+                    Aceleração eixo X: ".$this->getAcx() ."<br>
+                    Aceleração eixo Y: ".$this->getAcy();;    
         }
     }
 ?>
--- teste.php
+++ teste.php
@@ -1,9 +1,13 @@
 <?php
-require_once('classe/autoload.class.php');
-$temperature = isset($_GET['temperature']) ? $_GET['temperature'] : "";
-if(isset($temperature)) {
-      $temperatura = new Temperatura(0,$temperature);
-      $temperatura->Salvar();
-      echo $temperatura;
-   }
+   require_once('classe/autoload.class.php');
+   $temperature = isset($_GET['temperature']) ? $_GET['temperature'] : "";
+   $acx = isset($_GET['acx']) ? $_GET['acx'] : "";
+   $acy = isset($_GET['acy']) ? $_GET['acy'] : "";
+   $gyx = isset($_GET['gyx']) ? $_GET['gyx'] : "";
+   $gyy = isset($_GET['gyy']) ? $_GET['gyy'] : "";
+
+   if(isset($temperature) && isset($acx) && isset($acy) && isset($gyx) && isset($gyy)){
+         $temperatura = new Temperatura(0,$temperature,$acx,$acy,$gyx,$gyy);
+         $temperatura->Salvar();
+      }
 ?>
